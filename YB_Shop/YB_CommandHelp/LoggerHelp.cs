f8723e3506b7d32c61c9706822bab823eb8a0b05
jinqiang.zhu <jinqiang.zhu@chukou1.com>
2016-10-25 10:59:05 +0800

    update: Add log4net

---
+++ YB_Shop/YB_CommandHelp/LoggerHelp.cs
@@ -0,0 +1,28 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YB_CommandHelp
+{
+    public static class LoggerHelp
+    {
+        public static ILog m_Log;
+
+         static LoggerHelp()
+        {
+            m_Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        }
+        /// <summary>
+        /// 异步 error message
+        /// </summary>
+        /// <param name="message"></param>
+        public static void Log(string message)
+        {
+            Task.Run(()=>m_Log.Warn(message));
+        }
+    }
+}
